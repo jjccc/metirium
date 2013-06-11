bc323f5a181678ea214447061428be31204c1f52
unknown <Juanjo@JJCCC.(none)>
2013-06-11 23:37:23 +0200

    close #6

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.0'
 
 gem 'pg'
 gem "rake", "0.8.7"
+gem "ValidateEmail", "~> 1.0.0"
 
 
 
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,7 +1,17 @@
 # encoding: utf-8
+require 'rubygems'
+require 'ValidateEmail'
+
 class Lead < ActiveRecord::Base
   validates_presence_of :name, :message => "El nombre no puede estar vacío" 
   validates_presence_of :email, :message => "El correo electrónico no puede estar vacío"
   validates :name, :length => { :maximum => 150, :too_long => "El nombre no puede tener más de %{count} caracteres" }
   validates :email, :length => { :maximum => 250, :too_long => "El correo electrónico no puede tener más de %{count} caracteres" }
+  validate :mail_cannot_be_wrong, :on => :create
+  
+  def mail_cannot_be_wrong
+    if !ValidateEmail.validate(self.email.strip, true)
+      errors.add(:email, "La dirección especificada no es una dirección de correo válida")
+    end
+  end 
 end

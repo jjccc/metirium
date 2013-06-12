d00575c780dae07e963f39eb678d0cc08c53e912
unknown <Juanjo@JJCCC.(none)>
2013-06-13 00:07:35 +0200

    close #15

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def link_to_twitter
+    link_to "Seguir a @metirium", "https://twitter.com/metirium", :class => "twitter-follow-button", :"data-show-count" => "false", :"data-lang" => "es" 
+  end
 end

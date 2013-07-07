66d2fa2140fde7db54bdda20607c1e5d29f51e33
unknown <Juanjo@JJCCC.(none)>
2013-07-07 22:52:24 +0200

    close #41

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
   
   def after_sign_in_path_for(resource_or_scope)
     user_path(current_user)

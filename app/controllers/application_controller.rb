3138f01100e73d933ffcbef361a783c7e62b84a7
unknown <Juanjo@JJCCC.(none)>
2013-08-15 16:00:26 +0200

    close #79

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_locale
+  before_filter :set_locale, :set_root
  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
+  def set_root
+    @custom_root_path = root_path
+    @custom_root_path = user_path(current_user) if user_signed_in?
   end
   
   def after_sign_in_path_for(resource_or_scope)

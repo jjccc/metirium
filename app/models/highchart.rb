7254f915a7ed1c513cf32ea7f1adb09ea069bcd8
unknown <Juanjo@JJCCC.(none)>
2013-08-23 21:53:14 +0200

    close #90

--- app/models/highchart.rb
+++ app/models/highchart.rb
@@ -4,12 +4,15 @@ class Highchart
     @measurements = measurements.sort { |a, b| a.created_at <=> b.created_at }
   end
   
-  def data    
+  def data
     @measurements
   end
   
   def min
-    @measurements.map(&:amount).min
+    #sample_min = @measurements.map(&:amount).min
+    #sample_min = sample_min * 0.9 unless sample_min == 0         
+    #sample_min
+    0
   end
   
 end
